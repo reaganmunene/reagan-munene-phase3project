891e454751bf259fc2d37ec83a909cb4e72b25ab
reaganmunene <reagannmunene@gmail.com>
2024-05-08 12:37:22 +0300

    initialized sql dependancies and model file

---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Parent(Base):
+    __tablename__ = 'parents'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    children = relationship('Child', back_populates='parent')
+
+class Child(Base):
+    __tablename__ = 'children'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    parent_id = Column(Integer, ForeignKey('parents.id'))
+
+    parent = relationship('Parent', back_populates='children')
+
