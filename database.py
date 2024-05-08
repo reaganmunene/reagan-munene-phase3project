330ef5c14f9508e17ad5181b5298b8eac418d239
reaganmunene <reagannmunene@gmail.com>
2024-05-08 16:02:58 +0300

    Updated ORM files

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 # database.py
 
+import sqlalchemy
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
+sqlalchemy.orm.declarative_base()
+
 
 Base = declarative_base()
 
